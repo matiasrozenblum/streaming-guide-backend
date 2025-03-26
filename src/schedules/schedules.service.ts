cc2fa3b9c024ca28f5dc46054b3ae982d5d827e0
matiasrozenblum <rozenblummatias@gmail.com>
2025-03-26 11:12:01 -0300

    fix schedule

--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -22,28 +22,29 @@ export class SchedulesService {
   }
 
   async findOne(id: string): Promise<Schedule> {
-        const channel = await this.schedulesRepository.findOne({ where: { id } } as FindOneOptions );
-            if (!channel) {
-              throw new NotFoundException(`Channel with ID ${id} not found`);
-            }
-            return channel;
+    const channel = await this.schedulesRepository.findOne({ where: { id } } as FindOneOptions );
+    if (!channel) {
+      throw new NotFoundException(`Channel with ID ${id} not found`);
     }
+    return channel;
+  }
 
-    async create(createScheduleDto: CreateScheduleDto): Promise<Schedule> {
-      const program = await this.programsRepository.findOne({
-        where: { id: parseInt(createScheduleDto.programId, 10) },
-      });
-    
-      if (!program) {
-        throw new NotFoundException(`Program with ID ${createScheduleDto.programId} not found`);
-      }
-    
-      const schedule = this.schedulesRepository.create({
-        day_of_week: createScheduleDto.dayOfWeek,
-        start_time: createScheduleDto.startTime,
-        end_time: createScheduleDto.endTime,
-        program,
-      });
-    
-      return this.schedulesRepository.save(schedule);
+  async create(createScheduleDto: CreateScheduleDto): Promise<Schedule> {
+    const program = await this.programsRepository.findOne({
+      where: { id: parseInt(createScheduleDto.programId, 10) },
+    });
+  
+    if (!program) {
+      throw new NotFoundException(`Program with ID ${createScheduleDto.programId} not found`);
     }
+  
+    const schedule = this.schedulesRepository.create({
+      day_of_week: createScheduleDto.dayOfWeek,
+      start_time: createScheduleDto.startTime,
+      end_time: createScheduleDto.endTime,
+      program,
+    });
+  
+    return this.schedulesRepository.save(schedule);
+  }
+}
