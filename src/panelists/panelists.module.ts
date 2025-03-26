1153f4cd0773f8758abc17b3df05ce713937ef4a
matiasrozenblum <rozenblummatias@gmail.com>
2025-03-26 00:30:56 -0300

    Register controllers and services in modules

--- src/panelists/panelists.module.ts
+++ src/panelists/panelists.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PanelistsController } from './panelists.controller';
+import { PanelistsService } from './panelists.service';
+import { Panelist } from './panelists.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Panelist])],
+  controllers: [PanelistsController],
+  providers: [PanelistsService],
+})
 export class PanelistsModule {}
--- src/programs/programs.module.ts
+++ src/programs/programs.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProgramsController } from './programs.controller';
+import { ProgramsService } from './programs.service';
+import { Program } from './programs.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Program])],
+  controllers: [ProgramsController],
+  providers: [ProgramsService],
+})
 export class ProgramsModule {}
