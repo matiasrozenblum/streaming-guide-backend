cea9e2bfe7b6d61b117995fbdd28712fef23cceb
matiasrozenblum <rozenblummatias@gmail.com>
2025-03-26 11:17:54 -0300

    Fix: add Program to SchedulesModule

--- src/schedules/schedules.module.ts
+++ src/schedules/schedules.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SchedulesController } from './schedules.controller';
 import { SchedulesService } from './schedules.service';
 import { Schedule } from './schedules.entity';
+import { Program } from '../programs/programs.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Schedule])],
+  imports: [TypeOrmModule.forFeature([Schedule, Program])],
   controllers: [SchedulesController],
   providers: [SchedulesService],
 })
