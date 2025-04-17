7ce1ecbe66179f245db171e627a1c1d242d4502f
matiasrozenblum <rozenblummatias@gmail.com>
2025-04-16 23:14:37 -0300

    fix

--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -8,13 +8,8 @@ import { Cache } from 'cache-manager';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { UpdateScheduleDto } from './dto/update-schedule.dto';
 import dayjs from 'dayjs';
-import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
-import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import { YoutubeLiveService } from '../youtube/youtube-live.service';
 
-// Initialize dayjs plugins
-dayjs.extend(isSameOrAfter);
-dayjs.extend(isSameOrBefore);
 
 interface FindAllOptions {
   page?: number;
@@ -87,8 +82,6 @@ export class SchedulesService {
 
     const enriched = await Promise.all(
       data.map(async (schedule) => {
-        const start = dayjs(schedule.start_time, 'HH:mm');
-        const end = dayjs(schedule.end_time, 'HH:mm');
 
         let isLive = false;
         let streamUrl = schedule.program.youtube_url;
