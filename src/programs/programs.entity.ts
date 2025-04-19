c882c5cccbe7ef768337c1b84678948ba98ba91c
matiasrozenblum <rozenblummatias@gmail.com>
2025-04-19 00:56:24 -0300

    enabled cascading deletes for channels and programs

--- src/programs/programs.entity.ts
+++ src/programs/programs.entity.ts
@@ -18,7 +18,7 @@ export class Program {
   @JoinColumn({ name: 'channel_id' })
   channel: Channel;
 
-  @OneToMany(() => Schedule, (schedule) => schedule.program)
+  @OneToMany(() => Schedule, (schedule) => schedule.program, { cascade: true, onDelete: 'CASCADE' })
   schedules: Schedule[];
 
   @ManyToMany(() => Panelist, (panelist) => panelist.programs)
