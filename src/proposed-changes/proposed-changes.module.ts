7c93ec8de1b9fe9cc34637b7b45a2841ad07c3a5
Matias Rozenblum <37559797+matiasrozenblum@users.noreply.github.com>
2025-04-20 20:29:42 -0300

    Fix/proposed changes update (#54)
    * now applying proposed changes
    
    * fix imports
    
    * updated changelog and fixed tests

--- src/proposed-changes/proposed-changes.module.ts
+++ src/proposed-changes/proposed-changes.module.ts
@@ -3,8 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProposedChangesService } from './proposed-changes.service';
 import { ProposedChangesController } from './proposed-changes.controller';
 import { ProposedChange } from './proposed-changes.entity';
+import { Schedule } from '@/schedules/schedules.entity';
+import { Program } from '@/programs/programs.entity';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([ProposedChange])],
+  imports: [TypeOrmModule.forFeature([ProposedChange, Program, Schedule])],
   providers: [ProposedChangesService],
   controllers: [ProposedChangesController],
   exports: [ProposedChangesService],
