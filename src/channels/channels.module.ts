73e6aef115ac69b804bfe028e6ea7ddeec7aeddc
matiasrozenblum <rozenblummatias@gmail.com>
2025-04-21 15:05:40 -0300

    fix

--- src/channels/channels.module.ts
+++ src/channels/channels.module.ts
@@ -6,11 +6,13 @@ import { Channel } from './channels.entity';
 import { Program } from '../programs/programs.entity';
 import { Schedule } from '../schedules/schedules.entity';
 import { YoutubeLiveModule } from '../youtube/youtube-live.module';
+import { SchedulesModule } from '../schedules/schedules.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Channel, Program, Schedule]),
     forwardRef(() => YoutubeLiveModule),
+    forwardRef(() => SchedulesModule),
   ],
   controllers: [ChannelsController],
   providers: [ChannelsService],
