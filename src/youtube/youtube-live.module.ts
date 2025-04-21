845e4344175673050e9d64e828be37ae5ccf570c
matiasrozenblum <rozenblummatias@gmail.com>
2025-04-21 13:55:16 -0300

    implemented redis

--- src/youtube/youtube-live.module.ts
+++ src/youtube/youtube-live.module.ts
@@ -1,10 +1,21 @@
 import { Module, forwardRef } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { YoutubeLiveService } from './youtube-live.service';
 import { ConfigModule } from '../config/config.module';
 import { SchedulesModule } from '../schedules/schedules.module';
+import * as redisStore from 'cache-manager-redis-store';
+
 
 @Module({
-  imports: [ConfigModule, forwardRef(() => SchedulesModule)],
+  imports: [
+    ConfigModule,
+    forwardRef(() => SchedulesModule),
+    CacheModule.register({
+      store: redisStore as any,
+      url: process.env.REDIS_URL,
+      ttl: 1800,
+    }),
+  ],
   providers: [YoutubeLiveService],
   exports: [YoutubeLiveService],
 })
