e098172ba8985b7f6198e1ff291e94d3bcc3f0c1
matiasrozenblum <rozenblummatias@gmail.com>
2025-06-03 14:53:43 -0300

    fix expirations

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -43,7 +43,8 @@ export class AuthController {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'strict',
-        path: '/'
+        path: '/',
+        maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days in ms
       });
       return { access_token };
     } catch (err) {
