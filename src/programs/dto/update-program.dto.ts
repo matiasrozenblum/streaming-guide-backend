1f12d96c1da492e9eeb55f35ae00ccf0f1537642
matiasrozenblum <rozenblummatias@gmail.com>
2025-06-18 14:46:09 -0300

    adding style override for programs

--- src/programs/dto/update-program.dto.ts
+++ src/programs/dto/update-program.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateProgramDto } from './create-program.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateProgramDto extends PartialType(CreateProgramDto) {} 
+export class UpdateProgramDto extends PartialType(CreateProgramDto) {
+  @IsOptional()
+  @IsString()
+  style_override?: string;
+} 
